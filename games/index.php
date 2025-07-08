1f1a0812da5b44cd5ebbda44b74c9b5f2b35d798
Deploy <deploy@hw-recruit.com>
2025-07-08 10:01:53 +0000

    raw files in for the paged content

---
+++ games/index.php
@@ -0,0 +1,31 @@
+<?php include '/var/www/giraffian/includes/header.html'; ?>
+
+<h1 class="center">Games</h1>
+
+<p class="center">Small javascript games you can play with small children</p>
+
+<div class="tile">
+<a href="/kids/size"><img src="/images/flowerpot.png"></a>
+<h2><a href="/kids/size">Size</a></h2>
+<p>A sort-by-size game</p>
+</div>
+
+<div class="tile">
+<a href="/kids/shapes"><img src="/images/shapesthumb.png"></a>
+<h2><a href="/kids/shapes">Shapes & colors</a></h2>
+<p>A shapes and colours game.</p>
+</div>
+
+<div class="tile">
+<a href="/kids/count-on-your-fingers"><img src="/images/fingers.png"></a>
+<h2><a href="/kids/count-on-your-fingers">Count to ten</a></h2>
+<p>Count on your fingers game.</p>
+</div>
+
+<div class="tile">
+<a href="/kids/clock"><img src="/images/clockthumb.png"></a>
+<h2><a href="/kids/clock">Tell the time</a></h2>
+<p>An online application that helps teach children how to read an analogue clock.</p>
+</div>
+
+<?php include '/var/www/giraffian/includes/footer.html'; ?>
--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php include '/var/www/giraffian/includes/header.html'; ?>
+<?php include '/var/www/giraffian/includes/pagination.php'; ?>
 
-<h1>Giraffian</h1>
+<h1>Giraffian kid's stuff</h1>
 
+<div class="tile">
+<a href="/comic"><img src="/comic/images/weealot.png"</a>
+<h2><a href="/comic">Parenting comic</a></h2>
+<p>A pregnancy and parenting comic.</p>
+</div>
+
+<div class="tile">
+<a href="/kids/books"><img src="/images/alphacover.png"></a>
+<h2><a href="/kids/books">Online books</a></h2>
+<p>Online books for small children.</p>
+</div>
+
+<div class="tile">
+<a href="/games"><img src="/images/clockthumb.png"></a>
+<h2><a href="/games">Games</a></h2>
+<p>Simple games to play with your child.</p>
+</div>
+
+
+<h1 class="indent"><img src="/images/joke-icon.png">Jokes for little kids</h1>
+
+<?php random5('/var/www/giraffian/jokes'); ?>
+<p class="center"><a href="/jokes" class="button coral">View all jokes</a></p>
 <?php include '/var/www/giraffian/includes/footer.html'; ?>
