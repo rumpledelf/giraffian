fe411de7c6de3e73b2ddb8693a98ecf7393c555c
RumpledElf <robyn@armadillosandwich.com>
2025-11-21 03:03:37 +0000

    grr

---
+++ includes/header.php
@@ -0,0 +1,43 @@
+<?php include '/var/www/giraffian/includes/variables.php'; ?>
+<!DOCTYPE html>
+	<head>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NXJ6DGQ8');</script>
+		<!-- End Google Tag Manager -->
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0062762197282708" crossorigin="anonymous"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title><?php echo $sitename." | ".TITLE; ?></title>
+                <link rel="icon" type="image/x-icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+                <link rel="manifest" href="/site.webmanifest">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Comic+Relief:wght@400;700&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="/styles/style.css">
+	</head>
+	<body>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.php?id=GTM-NXJ6DGQ8"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+		<div id="wrapper">
+		<div id="header">
+			<div id="logo">
+				<a href="/"><img src="/images/gir-logo.png"></a>
+			</div>
+			<p><small><em>kid's educational activities, a parenting webcomic and other fun things for children and parents alike.</em></small></p>
+			<div id="menu">
+				<ul>
+					<li><a class="button coral" href="/jokes">jokes</a></li>
+					<li><a class="button yellow" href="/comic">comic</a></li>
+					<li><a class="button green" href="/words">words</a></li>
+					<li><a class="button aqua" href="/numbers">numbers</a></li>
+					<li><a class="button blue" href="/pictures">pictures</a></li>
+					<li><a class="button darkpurple" href="/kids/books">books</a></li>
+					<li><a class="button purple" href="/parents">for parents</a></li>
+					<li><a class="button grey" href="/about">about</a></li>
+				</ul>
+			</div>
+		</div>
