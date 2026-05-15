2b0a9f6aeee74cc005ad0a22dac281690616daac
RumpledElf <robyn@armadillosandwich.com>
2026-05-15 03:25:38 +0000

    Tweaked 404 page

--- 404.php
+++ 404.php
@@ -1,4 +1,37 @@
-<?php include '/var/www/giraffian/includes/header.php'; ?>
+<!DOCTYPE html>
+        <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Page not found</title>
+                <link rel="icon" type="image/x-icon" href="/favicon.ico">
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+                <link rel="manifest" href="/site.webmanifest">
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Comic+Relief:wght@400;700&display=swap" rel="stylesheet">
+                <link rel="stylesheet" href="/styles/style.css">
+        </head>
+        <body>
+                <div id="wrapper">
+                <div id="header">
+                        <div id="logo">
+                                <a href="/"><img src="/images/gir-logo.png"></a>
+                        </div>
+                        <p><small><em>kid's educational activities, a parenting webcomic and other fun things for children and parents alike.</em></small></p>
+                        <div id="menu">
+                                <ul>
+                                        <li><a class="button coral" href="/jokes">jokes</a></li>
+                                        <li><a class="button yellow" href="/comic">comic</a></li>
+                                        <li><a class="button green" href="/words">words</a></li>
+                                        <li><a class="button aqua" href="/numbers">numbers</a></li>
+                                        <li><a class="button blue" href="/pictures">pictures</a></li>
+                                        <li><a class="button darkpurple" href="/kids/books">books</a></li>
+                                        <li><a class="button purple" href="/parents">for parents</a></li>
+                                        <li><a class="button grey" href="/about">about</a></li>
+                                </ul>
+                        </div>
+                </div>
 
 <h1>Page not found</h1>
 
